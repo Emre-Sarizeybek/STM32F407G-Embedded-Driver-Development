6bf9f40361cecdad6a5493a1b4e6794af76831b8
Emre-Sarizeybek <srzybk.emre@gmail.com>
2020-01-21 12:27:23 +0300

    Peripheral clock enable and disable macros for GPIOx were added

--- stm32f407xx_drivers/drivers/inc/stm32f407xx.h
+++ stm32f407xx_drivers/drivers/inc/stm32f407xx.h
@@ -119,8 +119,36 @@ typedef struct{
 #define RCC   ((RCC_RegDef_t*) RCC_BASEADDR)
 
 
+/*
+ * Clock Enable Macros for GPIOx Peripherals
+ */
+#define GPIOA_PCLK_EN() (RCC->AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN() (RCC->AHB1ENR |= (1 << 1))
+#define GPIOC_PCLK_EN() (RCC->AHB1ENR |= (1 << 2))
+#define GPIOD_PCLK_EN() (RCC->AHB1ENR |= (1 << 3))
+#define GPIOE_PCLK_EN() (RCC->AHB1ENR |= (1 << 4))
+#define GPIOF_PCLK_EN() (RCC->AHB1ENR |= (1 << 5))
+#define GPIOG_PCLK_EN() (RCC->AHB1ENR |= (1 << 6))
+#define GPIOH_PCLK_EN() (RCC->AHB1ENR |= (1 << 7))
+#define GPIOI_PCLK_EN() (RCC->AHB1ENR |= (1 << 8))
+#define GPIOJ_PCLK_EN() (RCC->AHB1ENR |= (1 << 9))
+#define GPIOK_PCLK_EN() (RCC->AHB1ENR |= (1 << 10))
 
 
+/*
+ * Clock Disable Macros for GPIOx Peripherals
+ */
+#define GPIOA_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 0))
+#define GPIOB_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 1))
+#define GPIOC_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 2))
+#define GPIOD_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 3))
+#define GPIOE_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 4))
+#define GPIOF_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 5))
+#define GPIOG_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 6))
+#define GPIOH_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 7))
+#define GPIOI_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 8))
+#define GPIOJ_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 9))
+#define GPIOK_PCLK_DI() (RCC->AHB1ENR &= ~(1 << 10))
 
 
 
