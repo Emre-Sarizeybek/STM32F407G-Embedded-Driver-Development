1935d9b1e9277a23160b5d3401ab0e3d3d33f573
Emre-Sarizeybek <srzybk.emre@gmail.com>
2020-01-21 14:24:17 +0300

    Some generic macros were added

--- stm32f407xx_drivers/drivers/inc/stm32f407xx_gpio.h
+++ stm32f407xx_drivers/drivers/inc/stm32f407xx_gpio.h
@@ -10,6 +10,69 @@
 
 #include "stm32f407xx.h"
 
+
+/*
+ * @GPIO_PIN_NUMBERS
+ * GPIO pin numbers
+ */
+#define GPIO_PIN_NO_0    0
+#define GPIO_PIN_NO_1    1
+#define GPIO_PIN_NO_2    2
+#define GPIO_PIN_NO_3    3
+#define GPIO_PIN_NO_4    4
+#define GPIO_PIN_NO_5    5
+#define GPIO_PIN_NO_6    6
+#define GPIO_PIN_NO_7    7
+#define GPIO_PIN_NO_8    8
+#define GPIO_PIN_NO_9    9
+#define GPIO_PIN_NO_10  10
+#define GPIO_PIN_NO_11  11
+#define GPIO_PIN_NO_12  12
+#define GPIO_PIN_NO_13  13
+#define GPIO_PIN_NO_14  14
+#define GPIO_PIN_NO_15  15
+
+
+/*
+ * @GPIO_PIN_MODES
+ * GPIO possible modes
+ */
+#define GPIO_MODE_IN     0
+#define GPIO_MODE_OUT    1
+#define GPIO_MODE_ALTFN  2
+#define GPIO_MODE_ANALOG 3
+#define GPIO_MODE_IT_FT  4
+#define GPIO_MODE_IT_RT  5
+#define GPIO_MODE_IT_RFT 6
+
+
+/*
+ * @GPIO_PIN_OP_TYPE
+ * GPIO pin possible output modes
+ */
+#define GPIO_OP_TYPE_PP  0
+#define GPIO_OP_TYPE_OD  1
+
+
+/*
+ * @GPIO_PIN_SPEED
+ * GPIO pin possible output speeds
+ */
+#define GPIO_SPEED_LOW    0
+#define GPIO_SPEED_MEDIUM 1
+#define GPIO_SPEED_FAST   2
+#define GPIO_SPEED_HIGH   3
+
+
+/*
+ * @GPIO_PIN_PP_PD_TYPE
+ * GPIO pin pull up and pull down configuration macros
+ */
+#define GPIO_NO_PUPD       0
+#define GPIO_PIN_PU        1
+#define GPIO_PIN_PD        2
+
+
 /*
  * This is a configuration structure for a GPIOx pin
  */
@@ -41,7 +104,6 @@ typedef struct{
  * This is a peripheral clock control function for GPIO ports
  */
 void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
-
 
 
 #endif /* INC_STM32F407XX_GPIO_H_ */
